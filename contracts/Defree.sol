16ef9e9b54d70c4329037668dcb4f5630e29787b
Flavio Gouvea <46804396+freitasgouvea@users.noreply.github.com>
2020-10-23 09:11:02 -0300

    Update Defree.sol

--- contracts/Defree.sol
+++ contracts/Defree.sol
@@ -1,19 +1,136 @@
-// SPDX-License-Identifier: MIT
-// Implementation Based on: https://github.com/centrehq/centre-tokens/tree/master/contracts/v2
+//SPDX-License-Identifier: MIT
+// Implementation Based on USDC-EIP-3009: https://github.com/CoinbaseStablecoin/eip-3009/tree/master/contracts
 
 pragma solidity 0.6.12;
 
-//import "./ownership//ownable.sol";  
-//import "./lifecycle/pausable.sol";  
-import { IERC20_EIP3009 } from "./interfaces/IERC20_EIP3009.sol";
+/**
+ * @dev Interface of the ERC20 - EIP standard as defined in the EIP.
+ */
+interface IERC20_EIP3009 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
 
-contract Defree {
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    //EIP-3009
+
+    // keccak256("TransferWithAuthorization(address from,address to,uint256 value,uint256 validAfter,uint256 validBefore,bytes32 nonce)")
+    //bytes32 public constant TRANSFER_WITH_AUTHORIZATION_TYPEHASH = 0x7c7c6cdb67a18743f49ec6fa9b35f50d52ed05cbed4cc592e13b44501c1a2267;
+
+    event AuthorizationUsed(address indexed authorizer, bytes32 indexed nonce);
+
+    //string internal constant _INVALID_SIGNATURE_ERROR = "EIP3009: invalid signature";
+
+    /**
+     * @notice Returns the state of an authorization
+     * @param authorizer    Authorizer's address
+     * @param nonce         Nonce of the authorization
+     * @return True if the nonce is used
+     */
+    function authorizationState(address authorizer, bytes32 nonce)
+        external
+        view
+        returns (bool);
+
+    /**
+     * @notice Transfer tokens with a signed authorization
+     * @param from          Payer's address (Authorizer)
+     * @param to            Payee's address
+     * @param value         Amount to be transferred
+     * @param validAfter    The time after which this is valid (unix time)
+     * @param validBefore   The time before which this is valid (unix time)
+     * @param nonce         Unique nonce
+     * @param v             v of the signature
+     * @param r             r of the signature
+     * @param s             s of the signature
+     */
+    function transferWithAuthorization(
+        address from,
+        address to,
+        uint256 value,
+        uint256 validAfter,
+        uint256 validBefore,
+        bytes32 nonce,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+
+}
+
+contract Tides {
     
     //address of pool wallet
     address DefreePoolWallet;
     
     //IERC20_EIP3009 public ERC20Interface;
     address public USDCAddress;
+
+    event TransferFailed(address indexed authorizer, bytes32 indexed nonce);
 
     // (address,address,uint256,uint256,uint256,bytes32) = 20*2 + 32*4 = 168
     uint256 private constant _TRANSFER_PARAM_SIZE = 168;
@@ -22,13 +139,10 @@ contract Defree {
     // keccak256("transferWithAuthorization(address,address,uint256,uint256,uint256,bytes32,uint8,bytes32,bytes32)")[0:4]
     bytes4 private constant _TRANSFER_WITH_AUTHORIZATION_SELECTOR = 0xe3ee160e;
 
-    event TransferFailed(address indexed authorizer, bytes32 indexed nonce);
-
     event DepositSuccessful(address indexed _from, address indexed _to, uint256 _amount);  
     
     constructor(address _USDCAddress) public {
         USDCAddress = _USDCAddress;
-        //ERC20Interface = IERC20_EIP3009(USDCAddress);
     }
 
    function transferWithMultipleAuthorizations(
@@ -99,10 +213,14 @@ contract Defree {
                 );
                 emit TransferFailed(from, nonce);
             }
+            
+            SendGasTips(msg.sender);
+            
         }
 
         // Return true if all transfers were successful
         return numSuccessful == num;
+        
     }
 
     /**
@@ -151,5 +269,20 @@ contract Defree {
         revert(reason);
     }
     
+    /*
+     * @dev Send Gas Tips to the Caller of transferWithMultipleAuthorizations
+     * CAUTION: under development. This function transfers the balance of this contract to the msg.sender 
+     * of the transfer with authorization function. In case of more than one simultaneous call the transfer 
+     * of the gas tip may fail.
+     * @param msg.sender of transferWithMultipleAuthorizations (address)
+     * @return True (boolean)
+     */
+    function SendGasTips(address caller) public payable returns(bool){
+       uint256 balance = IERC20_EIP3009(USDCAddress).balanceOf(address(this));
+       IERC20_EIP3009(USDCAddress).transfer(caller, balance);
+       return true;
+    }
+    
     receive() payable external {}
+    
 }
